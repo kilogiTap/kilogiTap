c942c3b6cd4890f231952cbf2d9ff2e2b5b41952
kilogiTap <136350583+kilogiTap@users.noreply.github.com>
2024-07-11 00:54:12 +0800

    Create xuy.py

---
+++ xuy.py
@@ -0,0 +1,15 @@
+# meta developer: @modwini
+from .. import loader
+from asyncio import sleep
+
+
+@loader.tds
+class rap(loader.Module):
+    strings = {"name": "hityka"}
+
+    @loader.owner
+    async def rapcmd(self, message):
+        for _ in range(1):
+            for rap in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня"]:
+                await message.edit(bro)
+                await sleep(0.3)
