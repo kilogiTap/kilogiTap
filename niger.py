693dbe0c442d74551b00e65e9779459d158f4c21
kilogiTap <136350583+kilogiTap@users.noreply.github.com>
2024-07-11 00:49:07 +0800

    Update niger.py

--- niger.py
+++ niger.py
@@ -1,15 +1,15 @@
-# meta developer: @modwini
+# meta developer: @hityka
 from .. import loader
 from asyncio import sleep
 
 
 @loader.tds
-class rap(loader.Module):
+class bro(loader.Module):
     strings = {"name": "hityka"}
 
     @loader.owner
-    async def rapcmd(self, message):
+    async def brocmd(self, message):
         for _ in range(1):
-            for rap in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня💋"]:
-                await message.edit(rap)
+            for bro in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня)"]:
+                await message.edit(bro)
                 await sleep(0.3)
