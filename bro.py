48404703fff94b48ebf6670eecb86afa4bbe4b6c
kilogiTap <136350583+kilogiTap@users.noreply.github.com>
2024-07-11 00:39:13 +0800

    Update bro.py

--- bro.py
+++ bro.py
@@ -4,12 +4,12 @@ from asyncio import sleep
 
 
 @loader.tds
-class rap(loader.Module):
+class bro(loader.Module):
     strings = {"name": "hityka"}
 
     @loader.owner
     async def rapcmd(self, message):
         for _ in range(1):
-            for rap in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня💋"]:
+            for bro in ["бро ты...", лучший", "отмосферный", "кайфовый", " вайбовый", "никогда не бросай меня💋"]:
                 await message.edit(bro)
                 await sleep(0.3)
