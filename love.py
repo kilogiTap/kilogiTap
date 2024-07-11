469cbaec8a2c00fa2c6cccb2380013dbbf233519
kilogiTap <136350583+kilogiTap@users.noreply.github.com>
2024-07-11 21:41:42 +0800

    Create love.py

---
+++ love.py
@@ -0,0 +1,16 @@
+# meta developer: @hityka
+from .. import loader
+from asyncio import sleep
+
+
+@loader.tds
+class rap(loader.Module):
+    strings = {"name": "влад пидор"}
+
+    @loader.owner
+    async def rapcmd(self, message):
+        for _ in range(1):
+            for rap in ["ты..  ", "лучшая", "самая красивая", "очень добрая", " вайбовая", "и", "я",
+"тебя", "очень", "люблю"]:
+                await message.edit(rap)
+                await sleep(0.3)
